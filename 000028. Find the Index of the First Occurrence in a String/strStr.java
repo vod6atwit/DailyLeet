f3ee38cea5a9c3263af6205c29601c62c902a130
Duyvo1117 <duyvo1117@gmail.com>
2023-08-10 12:00:02 -0400

    Runtime: 0ms(100%) - Memory: 40.16MB(96.08%)

---
+++ 000028. Find the Index of the First Occurrence in a String/strStr.java
@@ -0,0 +1,11 @@
+class Solution {
+  public int strStr(String haystack, String needle) {
+    return haystack.indexOf(needle);
+  }
+}
+
+// two pointer problem
+// Runtime : 0 ms - Beats: 100%
+// Memory : 40.16 MB - Beats: 96.08%
+// Link submission:
+// https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/submissions/1017672194/
