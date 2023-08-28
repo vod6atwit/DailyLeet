435aac8503f2fcb55619a1037582bead6fdd71ed
Duyvo1117 <duyvo1117@gmail.com>
2023-08-28 17:00:54 -0400

    Runtime: 0ms(100%) - Memory: 38.9MB(83.32%)

---
+++ 001025. Divisor Game/divisorGame.java
@@ -0,0 +1,13 @@
+class Solution {
+  public boolean divisorGame(int n) {
+    if (n % 2 == 0) {
+      return true;
+    }
+    return false;
+  }
+}
+
+// Runtime : 0 ms - Beats: 100%
+// Memory : 38.9 MB - Beats: 83.32%
+// Link submission:
+// https://leetcode.com/problems/divisor-game/submissions/1034479914/
