371eceabbcde36b279926c814d68c893a76e80d0
Duyvo1117 <duyvo1117@gmail.com>
2024-03-23 12:58:54 -0400

    Runtime: 58ms(62.43%) - Memory: 51.62MB(53.51%)

---
+++ 002665. Counter II/createCounter.js
@@ -0,0 +1,19 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let temp = init;
+    return {
+      increment: function() {return ++temp},
+      decrement: function() {return --temp},
+      reset: function() {return temp = init}
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
