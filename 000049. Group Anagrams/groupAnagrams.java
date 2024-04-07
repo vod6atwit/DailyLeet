1bc098d632c2ea232ac1c761bcdc8bd31e77938f
Duyvo1117 <duyvo1117@gmail.com>
2024-04-07 16:49:46 -0400

    Runtime: 7ms(65.02%) - Memory: 47.65MB(62.15%)

--- 000049. Group Anagrams/groupAnagrams.java
+++ 000049. Group Anagrams/groupAnagrams.java
@@ -1,54 +1,26 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List> result = new HashMap<>();
-        int[] myArray = new int[26];
-
-        for(int i = 0; i < strs.length; i++){
-            Arrays.fill(myArray, 0);
-            //             for(int z : myArray){
-            //     System.out.print(z + " ");
-            // }
-            // System.out.println("");
-            for(int j = 0; j < strs[i].length(); j++){
-                char c = strs[i].charAt(j);
-
-                // System.out.println(myList.get(c-'a'));
-                myArray[c-'a'] += 1;
-            }
-
-            // for(int z : myArray){
-            //     System.out.print(z + " ");
-            // }
-            // System.out.println("");
-            // result.put(myArray, strs[i]);
-            // System.out.println(result.get(myArray));
-            String s = Arrays.toString(myArray);
-            // System.out.println(s);
-            if(!result.containsKey(s)){
-                List<String> newList = new ArrayList<>();
-                newList.add(strs[i]);
-                result.put(s,newList);
-            }else{
-                List<String> existingList = result.get(s);
-                existingList.add(strs[i]);
-                // System.out.println(result.get(s));
-            }
-            // System.out.println(result.get(s));
-            // for (List<String> values : result.values()) {
-            //     System.out.println(values);
-            // }
+        Map<String, List<String>> map = new HashMap<>();
+        for (int i = 0; i < strs.length; i++) {
+            char[] char_arr = strs[i].toCharArray();
+            Arrays.sort(char_arr);
+            String str = new String(char_arr);
+            
+            if (!map.containsKey(str)) {
+                map.put(str, new ArrayList<>());
+            } 
+            map.get(str).add(strs[i]);
+            
         }
 
-        List<List<String>> finalResult = new ArrayList<>();
-        
-        for (List<String> values : result.values()) {
-            finalResult.add(values);
-        }
-
-        return finalResult;
+        // List<List<String>> result = new ArrayList<>();
+        // for(List<Integer> l : map.values()){
+        //     List<String> str_list = new ArrayList<>();
+        //     for(int i : l){
+        //         str_list.add(strs[i]);
+        //     }
+        //     result.add(str_list);
+        // }
+        return new ArrayList<>(map.values());
     }
 }
-
-// Runtime : 20ms - Beats: 14.78%
-// Memory : 47.76MB - Beats: 40.99%
-// Link submission: https://leetcode.com/problems/group-anagrams/submissions/1173488065
