d4ca9a0e5e4bfc252d565fbc6a3b244017d2fc83
Duyvo1117 <duyvo1117@gmail.com>
2024-04-13 10:14:02 -0400

    Runtime: 1301ms(50.60%)

---
+++ 001398. Customers Who Bought Products A and B but Not C/queries.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT c.customer_id, customer_name
+FROM customers c
+LEFT JOIN orders o ON c.customer_id = o.customer_id
+GROUP BY c.customer_id
+HAVING 
+    SUM(product_name='A') > 0
+    AND SUM(product_name='B') > 0
+    AND SUM(product_name='C') = 0
+ORDER BY c.customer_id;
+
+ 
